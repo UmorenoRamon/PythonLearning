0d1bd4282e5e15fb42ec434f880caac192512f2c
Ulises Moreno Ramon <36385858+UmorenoRamon@users.noreply.github.com>
2026-06-02 16:56:44 +0000

    Initial commit added to python files

---
+++ copied.py
@@ -0,0 +1,11 @@
+list1 = [234, 565, 796, True, "Python", 685.34, 565, 234]
+print(list1)
+print(len(list1))
+print(list1[-1])
+print(list1[3:9]) #slicing
+print(list1[-6:-2])
+print(list1[::-1])
+list1[0] = 100
+print(list1)
+list1.append(400) #will add element at the end
+print(list1)
